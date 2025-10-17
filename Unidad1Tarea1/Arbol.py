5763175472bd1bcd391aabaea829250362b45f52
hanscle777 <149011011+hanscle777@users.noreply.github.com>
2025-10-17 18:09:56 -0400

    Add Arbol class with initialization and height method

--- Unidad1Tarea1/Arbol.py
+++ Unidad1Tarea1/Arbol.py
@@ -1,1 +1,14 @@
-temp
+from Nodo import Nodo
+class Arbol:
+    __raiz__ = None
+    __n__ = 0
+
+    def __init__(self):
+        self.__raiz__ = None
+        self.__n__ = 0
+        
+    def __getAltura(self, nodo):
+        if nodo is None:
+            return 0
+        return nodo.getAltura()
+
