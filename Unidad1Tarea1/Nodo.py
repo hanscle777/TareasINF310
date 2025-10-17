03264166cae1f3f4b6d22dba33148a46e8624ae4
hanscle777 <149011011+hanscle777@users.noreply.github.com>
2025-10-17 19:33:19 -0400

    Remove Arbol class from Nodo.py
    Removed the Arbol class and its associated methods.

--- Unidad1Tarea1/Nodo.py
+++ Unidad1Tarea1/Nodo.py
@@ -39,16 +39,5 @@ class Nodo:
         self.__altura__ = altura
 
 
-class Arbol:
-    __raiz__ = None
-    __n__ = 0
 
-    def __init__(self):
-        self.__raiz__ = None
-        self.__n__ = 0
-        
-    def __getAltura(self, nodo):
-        if nodo is None:
-            return 0
-        return nodo.getAltura()
 
